d427c6d9326e09ba66150392f146aa922f51f1a5
TsiraMesa <ciramesablishvili@gmail.com>
2024-08-27 23:33:00 +0400

    finaluri davaleba

---
+++ cypress/e2e/test 1 .cy.js
@@ -0,0 +1,32 @@
+
+//მომხმარებლის რეგისტრაცია ახალი მონაცემებით და პერსონალური მონაცემების სემოწმება.
+
+import userData from '../fixtures/example.json'
+describe('registration', () => {
+    it('login', () => {
+        cy.visit('https://testzootopia.loremipsum.ge/ka')
+
+        let random = Math.floor(Math.random() * 1000)
+
+        cy.get('.menu-pop > .rprof').click()
+        cy.get('.input-shablon > p > a').should("be.visible").click()
+        cy.contains("რეგისტრაცია").should("be.visible")
+        cy.login(userData.name, userData.id, userData.mob, userData.password, userData.password1)
+        cy.get(':nth-child(2) > .imail').type  ("ciramesablishvili "+ random +" @gmail.com")
+        cy.get("label:nth-child(2) > svg:nth-child(1) > g:nth-child(1) > rect:nth-child(1)").click({ force: true })
+        cy.get('.regsub').click()
+        cy.get('.bg-transparent').click()
+        cy.contains("პროფილი").should("be.visible").click()
+        cy.get('h1').should("have.text","პერსონალური ინფორმაცია")
+        
+
+
+
+
+
+
+
+
+
+    })
+})
